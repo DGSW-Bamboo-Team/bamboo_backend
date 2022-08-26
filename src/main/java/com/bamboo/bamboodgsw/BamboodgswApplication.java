33a1cf870bf0d28ee4a2baa97132b77b1fd319b6
whitebear05 <woojs0505@naver.com>
2022-08-26 19:28:23 +0900

    :tada: :: start project

---
+++ src/main/java/com/bamboo/bamboodgsw/BamboodgswApplication.java
@@ -0,0 +1,13 @@
+package com.bamboo.bamboodgsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamboodgswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BamboodgswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamboo/bamboodgsw/BamboodgswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamboo.bamboodgsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamboodgswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
