4f0cb224c898a10b6402d5345fd90200ea706c08
yongbin <rhdydqls1024@naver.com>
2022-09-06 09:27:51 +0900

    :fire: :: upload 코드 삭제

--- src/main/java/com/bamboo/bamboodgsw/domain/upload/repository/PostAttachmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bamboo.bamboodgsw.domain.post.repository;
-
-import com.bamboo.bamboodgsw.domain.post.entity.PostAttachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostAttachmentRepository extends JpaRepository<PostAttachment, Long> {
-}
